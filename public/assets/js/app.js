c43bf58e765966727842028c54e90f1ad369c949
Priya Balakrishnan <andu_pri@yahoo.com>
2017-12-08 17:03:09 -0500

    feat: Add note and save data feature

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,59 +1,132 @@
+$(document).ready(function(){
+
+  getData();
+
+// Scrape data from website
 $("#scrape").click(function(e){
   e.preventDefault();
-  $.ajax("/scrape", function(data) {
-    console.log(data);
+  $.get("/scrape", function(data) {
+    $("#main").empty();
+    $('.modal-title').text('News Scraper');
+    $('.modal-body').html("<p> Scraped "+ data.length+" new articles.</p>");
+    $('.modal').modal('toggle')
+    
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#main").append("<h4><a target='_blank' href='"+ data[i].link+"''>" + data[i].title + "</h4><p>" + data[i].summary + "</p>");
+      $("#main").append("<button class='mb-3 btn btn-dark' id='saveArticle'>Save Article</button>");
+    }
   });  
 });
 
-// $("#articles").click(function(e){
-  
-//   e.preventDefault();
-  
+// Display saved articles
+ $("#articles").click(function(e){
+  e.preventDefault();
+  getData();
+});
+
+ function getData() {
   // Grab the articles as a json
   $.getJSON('/articles', function(data) {
-    console.log("data");
+    $("#main").empty();
+    if(data.length === 0){
+      $("#main").append("<p>No saved data!</p>");
+    }else{
     //For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#main").append("<h4 data-id='" + data[i]._id + "'>" + data[i].headLine + "</h3><p>" + data[i].urlLink + "</p>");
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#main").append("<h4><a target='_blank' href='"+ data[i].urlLink+"''>" + data[i].headLine + "</h4><p>" + data[i].summary + "</p>");
+        $("#main").append("<button id='addNote' class='mb-3 btn btn-dark' data-id='"+data[i]._id+"'>Article Note</button><button id='deleteArticle'class='mb-3 ml-2 btn btn-dark' data-id='"+data[i]._id+"'>Delete Article</button>");
+      }
     }
    });
-//});
+ }
 
+// Save an article to the database
+$(document).on("click", "#saveArticle", function(){
+   // Run a POST request to save the article
+   var summary =$(this).prev().text();
+   var title =$(this).prev().prev().text();
+   var link = $(this).prev().prev().children().attr('href');
+    $.ajax({
+    method: "POST",
+    url: "/api/article/",
+     data: {
+      // Value taken from title input
+      headLine: title,
+      // Value taken from link
+      urlLink: link,
+      summary: summary
+      }
+    })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+      $('.modal-title').text('News Scraper');
+      $('.modal-body').html("<p>"+ data +"</p>");
+      $('.modal').modal('toggle')
+    });
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
+});
+
+// Delete an article
+$(document).on("click", "#deleteArticle", function(){
+  // Run a DELETE request to save the article
+  var id = $(this).attr("data-id");
+  $.ajax({
+    method: "DELETE",
+    url: "/delete/article/"+ id,
+    })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+      getData();
+    });
+
+});
+
+// Add a note
+$(document).on("click", "#addNote", function() {
+  $('.modal-title').text('Article Notes');
+  $('.modal').modal('toggle');
+
+  // Save the id from the button tag
   var thisId = $(this).attr("data-id");
 
   // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
-    url: "/articles/" + thisId
+    url: "/article/" + thisId
   })
     // With that done, add the note information to the page
     .done(function(data) {
+      $(".modal-body").html("");
       console.log(data);
-     
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      if(data.note.length===0){
+        $(".modal-body").append("<h5>Article has no notes!</h5>");
+      }else{
+        data.note.forEach(function(item){
+          // The title of the article
+          $("#notes").append("<li>" + item.title +":"+ item.body + "</li>");
+        });
+       }  
 
+      // An input to enter a new title
+        $(".modal-body").append("<input id='titleinput' name='title' class='mb-3'><br>");
+        // A textarea to add a new note body
+        $(".modal-body").append("<textarea id='bodyinput' name='body'></textarea><br>");
+        // A button to submit a new note, with the id of the article saved to it
+        $(".modal-body").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
       // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
+      // if (data.note) {
+      //   // Place the title of the note in the title input
+      //   $("#titleinput").val(data.note.title);
+      //   // Place the body of the note in the body textarea
+      //   $("#bodyinput").val(data.note.body);
         
-      }
+     // }
 
     });
 });
@@ -86,3 +159,4 @@ $(document).on("click", "#savenote", function() {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+})
--- server.js
+++ server.js
@@ -43,29 +43,38 @@ app.get("/scrape", function(req, res) {
   axios.get("https://www.wsj.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-    
+    var results = [];
+
     // Now, we grab every article tag, and do the following:
-    $(".wsj-card").each(function(i, element) {
+    $(".LS-SECONDARY-ALT").each(function(i, element) {
       // Save an empty result object
-      var result = {};
+      //var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.headLine = $(this).find('.wsj-headline').text();
-      result.urlLink = $(this).find('.wsj-headline').find('.wsj-headline-link').attr('href');
-      result.summary = $(this).find('.wsj-card-body').find('.wsj-summary').text();
+      // result.headLine = $(this).find('.wsj-headline').text();
+      // result.urlLink = $(this).find('.wsj-headline').find('.wsj-headline-link').attr('href');
+      // result.summary = $(this).find('.wsj-card-body').find('.wsj-summary').text();
       // Create a new Article using the `result` object built from scraping
-      db.Article
-        .create(result)
-        .then(function(dbArticle) {
-        	// If we were able to successfully scrape and save an Article, send a message to the client
-          console.log('scrape complete', dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          res.json(err);
-        });
+      // db.Article
+      //   .create(result)
+      //   .then(function(dbArticle) {
+      //   	// If we were able to successfully scrape and save an Article, send a message to the client
+      //     console.log('scrape complete', dbArticle);
+      //   })
+      //   .catch(function(err) {
+      //     // If an error occurred, send it to the client
+      //     res.json(err);
+      //   });
+      var title = $(this).find('.wsj-headline').text();
+      var link  = $(this).find('.wsj-headline').find('.wsj-headline-link').attr('href');
+      var summary = $(this).find('.wsj-card-body').find('.wsj-summary').text();
+      results.push({
+        title: title,
+        link: link,
+        summary: summary
+      });
     });
-    res.send('Scrape Complete');
+    res.json(results);
   });
 });
 
@@ -80,6 +89,73 @@ app.get("/articles", function(req, res) {
     })
     .catch(function(err) {
       // If an error occurs, send the error back to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/article/:id",function(req,res){
+  db.Article.findOne({"_id": req.params.id }).populate("note")
+    .then(function(dbArticle) {
+      // If any articles are found, send them to the client with any associated notes
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
+});
+
+// Route for saving article to database
+app.post("/api/article", function(req, res){
+  // Save the request body as an object called article
+  var article = req.body;
+
+  db.Article.create(article, function(err, articledata){
+    if (err) {
+      if (err.name === 'MongoError' && err.code === 11000) {
+        // Duplicate name
+         return res.send('Article already exists!');
+      }
+    }
+    res.send("Artice Saved!");
+  });
+});
+
+// Delete an article
+app.delete("/delete/article/:id", function(req, res) {
+  // Remember: when searching by an id, the id needs to be passed in
+  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
+  var id = req.params.id;
+  //console.log(id);
+  db.Article.deleteOne({ "_id": id }, 
+    function(err, deleted) {
+    // Log any errors if the server encounters one
+    if (err) throw err;
+       // Otherwise, send the result of this query to the browser
+      res.send(deleted);
+  });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+    // save the new note that gets posted to the Notes collection
+  // then find an article from the req.params.id
+  // and update it's "note" property with the _id of the new note
+  db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find the article and push the new Note's _id to the Articles's `notes` array
+      // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbNote._id } }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If the Article was updated successfully, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
       res.json(err);
     });
 });
